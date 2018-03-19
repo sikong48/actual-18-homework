d4c89a254d637d9609eaf5be1de5fa5c24c3a5b8
Pandar <77406458@qq.com>
2018-03-19 13:14:58 +0800

    commit homework

--- 01/zhangjun/game.py
+++ 01/zhangjun/game.py
@@ -6,13 +6,18 @@
 import random
 
 random_num = random.randint(0, 100)
-input_text = input('来猜个数吧，在控制台输入个0-100之间的整数：')
+input_text = int(input('来猜个数吧，5次机会，在控制台输入个0-100之间的整数：'))
 count = 1
-while int(input_text) != random_num:
+while True:
+    if input_text == random_num:
+        print('猜对了')
+        break
+    elif input_text > random_num:
+        print('猜大了,你已经猜了' + str(count) + '次，再猜：')
+    else:
+        print('猜小了,你已经猜了' + str(count) + '次，再猜：')
+    input_text = int(input())
+    count += 1
     if count == 5:
         print('太笨了，下次再来')
         break
-    input_text = input('不对，你已经猜了' + str(count) + '次，再给你一次机会：')
-    count += 1
-    if int(input_text) == random_num:
-        print('猜对了')
